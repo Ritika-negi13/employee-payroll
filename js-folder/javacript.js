5f4b190b8438a9512cbb9311607934438038ca7e
ritikanegi-dli0593 <ritika.negi@godigit.com>
2024-07-29 18:29:21 +0530

    [last modified : 29-july -2024 by: Ritika Negi ]

--- js-folder/javacript.js
+++ js-folder/javacript.js
@@ -22,3 +22,44 @@ function getEmplyeeList(){
             }
         })
 }
+
+function openPopup() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementsByClassName("backdrop")[0].style.display = "block";
+}
+  
+function closePopup() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementsByClassName("backdrop")[0].style.display = "none";
+}
+function deleteData(){
+    document.getElementById("ondelete").style.display = "";
+    console.log("delete")
+}
+function openSearchbar(){
+    document.getElementById("myInput").style.display =""; 
+}
+
+function onSearch() {
+    var input, filter, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table=document.getElementById("employeetable");
+    tr=table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        td2 =tr[i].getElementsByTagName("td")[2];
+        if (td || td2) {
+          txtValue = td.textContent || td.innerText;
+          txtgender= td2.textContent || td2.innerText;
+          console.log(filter);
+          if ( txtValue.toUpperCase().indexOf(filter) > -1 || txtgender.toUpperCase().startsWith(filter)==1) {
+            tr[i].style.display = "";
+          } 
+          else {
+            tr[i].style.display = "none";
+          }
+        }      
+    }
+}
